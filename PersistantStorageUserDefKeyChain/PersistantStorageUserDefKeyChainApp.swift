227ade24769635c2ec20caa890887c95bda46ba6
Manan Patel <manan.patel@outlook.com>
2024-09-02 12:42:28 -0600

    Initial Commit

---
+++ PersistantStorageUserDefKeyChain/PersistantStorageUserDefKeyChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistantStorageUserDefKeyChainApp.swift
+//  PersistantStorageUserDefKeyChain
+//
+//  Created by Manan Patel on 2024-09-02.
+//
+
+import SwiftUI
+
+@main
+struct PersistantStorageUserDefKeyChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
